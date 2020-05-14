61122a450f74c6d904475fd46d9be3b74ca22c0d
Antonioisrooted <antoniommaina@gmail.com>
2020-05-14 16:37:10 +0300

    App

---
+++ src/main/java/Restaurantservices/App.java
@@ -0,0 +1,107 @@
+import Restaurantservices.PostMeal;
+import Restaurantservices.PostRecipe;
+import spark.ModelAndView;
+import spark.Spark;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static spark.Spark.*;
+
+public class App {
+    static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null)
+        {return Integer.parseInt((String)processBuilder.environment().get("PORT")) ;}
+        return 4567;
+    }
+
+    public static void main(String[] args) {
+
+        Spark.port(getHerokuAssignedPort());
+
+        staticFileLocation("/public");
+
+
+
+        get("/", (req,res) -> {
+            Map<String,Object> model = new HashMap<String,Object>();
+            return new ModelAndView(model, "index.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        get("/sightings/form",(req,res)->{
+            Map<String,Object> model = new HashMap<String,Object>();
+            List<PostRecipe>normalAnimals=PostRecipe.all();
+            model.put("normalAnimals",normalAnimals);
+            List<PostMeal>endangeredAnimals=PostMeal.all();
+            model.put("endangeredAnimals",endangeredAnimals);
+            return new ModelAndView(model, "sightingform.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        post("/sightings/new",(req,res)->{
+            Map<String,Object> model = new HashMap<String,Object>();
+            int animalId = Integer.parseInt(req.queryParams("animalId"));
+            String rangerName = req.queryParams("rangerName");
+            String sightLocation = req.queryParams("sightLocation");
+            Odder Odderz = new Odder(animalId,sightLocation,rangerName);
+            sighting.save();
+            return new ModelAndView(model, "sightingz.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        get("/sightings",(req,res)->{
+            Map<String,Object> model = new HashMap<String,Object>();
+            model.put("sightings", Odder.all());
+            return new ModelAndView(model, "sightings.hbs");
+        }, new HandlebarsTemplateEngine());
+
+
+        get("/endangered",(req,res) ->{
+            Map<String,Object> model = new HashMap<String,Object>();
+            model.put("endangeredAnimals",PostMeal.all());
+            return new ModelAndView(model, "endangeredanimal.hbs");
+        }, new HandlebarsTemplateEngine());
+
+
+        get("/endangered/form",(req,res)->{
+            Map<String,Object> model = new HashMap<String,Object>();
+            return new ModelAndView(model, "endangeredform.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        post("/endangered/new",(req,res) ->{
+            Map<String,Object> model = new HashMap<String,Object>();
+            String name = req.queryParams("animalName");
+            String health = req.queryParams("description");
+            String age = req.queryParams("cook");
+            PostMeal animal= new PostMeal(name,description,cook);
+            animal.save();
+            return new ModelAndView(model, "endangeredz.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        get("/animal/form",(req,res) -> {
+            Map<String,Object> model = new HashMap<String,Object>();
+            return new ModelAndView(model, "animalform.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        post("/animal/new",(req,res) -> {
+            Map<String,Object> model = new HashMap<String,Object>();
+            String name = req.queryParams("animalName");
+            String health = req.queryParams("description");
+            String age = req.queryParams("cook");
+            PostRecipe animal= new PostRecipe(name,description,cook );
+            animal.save();
+            return new ModelAndView(model, "animalz.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        get("/animals",(req,res) ->{
+            Map<String,Object> model = new HashMap<String,Object>();
+            List<PostRecipe>normalAnimals=PostRecipe.all();
+            model.put("normalAnimals",normalAnimals);
+            return new ModelAndView(model, "animal.hbs");
+        }, new HandlebarsTemplateEngine());
+    }
+
+    private static void staticFileLocation(String s) {
+    }
+}
--- src/main/java/Restaurantservices/Odder.java
+++ src/main/java/Restaurantservices/Odder.java
@@ -1,4 +1,83 @@
-package Restaurantservices;
+import org.sql2o.*;
+
+import java.sql.Connection;
+import java.sql.Timestamp;
+
+import java.util.List;
+import java.util.Objects;
 
 public class Odder {
+    private int id;
+    private int animalId;
+    private String sightLocation;
+    private String rangerName;
+    private Timestamp viewed;
+
+    public Odder(int animalId, String sightLocation, String rangerName){
+        this.animalId = animalId;
+        this.sightLocation = sightLocation;
+        this.rangerName = rangerName;
+    }
+
+    public int getAnimalId(){
+        return animalId;
+    }
+    public String getSightLocation(){
+        return sightLocation;
+    }
+    public String getRangerName(){
+        return rangerName;
+    }
+
+    public int getId() {
+        return id;
+    }
+    public Timestamp getViewed(){
+
+        return viewed;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Odder sightings = (Odder) o;
+        return id == sightings.id &&
+                this.animalId == sightings.animalId &&
+                this.sightLocation.equals(sightings.sightLocation) &&
+                this.rangerName.equals(sightings.rangerName) ;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, animalId, sightLocation, rangerName );
+    }
+
+    public void save(){
+        try(Connection con = DB.sql2o.open()){
+            String sql = "INSERT INTO sightings (animalid, sightlocation, rangername) VALUES (:animalId, :sightLocation, :rangerName)";
+            this.id = (int) con.createQuery(sql,true)
+                    .addParameter("animalId",this.animalId)
+                    .addParameter("sightLocation",this.sightLocation)
+                    .addParameter("rangerName",this.rangerName)
+                    .executeUpdate()
+                    .getKey();
+        }
+    }
+    public static List<Odder> all(){
+        String sql = "SELECT * FROM sightings";
+        try(Connection con = DB.sql2o.open()){
+            return con.createQuery(sql)
+                    .executeAndFetch(Odder.class);
+        }
+    }
+    public static Odder find(int id){
+        try(Connection con = DB.sql2o.open()) {
+            String sql = "SELECT * FROM sightings WHERE id = :id";
+            Odder sighting = con.createQuery(sql)
+                    .addParameter("id",id)
+                    .executeAndFetchFirst(Odder.class);
+            return sighting;
+        }
+    }
 }
