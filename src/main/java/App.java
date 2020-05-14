b00d5d3f194640ebcf4779e9a1d3eb5d6f9c3968
Reuben <gathiireuben@gmail.com>
2020-05-15 00:48:58 +0300

    adds the App java class

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,4 +1,28 @@
-package PACKAGE_NAME;
+import java.util.HashMap;
+import java.util.Map;
+import spark.ModelAndView;
+import spark.Spark;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
+import static spark.Spark.*;
 
 public class App {
+    static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder(new String[0]);
+        return processBuilder.environment().get("PORT") != null ? Integer.parseInt((String)processBuilder.environment().get("PORT")) : 4567;
+    }
+
+    public static void main(String[] args) {
+
+        Spark.port(getHerokuAssignedPort());
+
+        staticFileLocation("/public");
+
+        Map<String,Object> model = new HashMap<String,Object>();
+
+        get("/", (req,res) -> {
+            return new ModelAndView(model, "layout.hbs");
+        }, new HandlebarsTemplateEngine());
+
+    }
 }
